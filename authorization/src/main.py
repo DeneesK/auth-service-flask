606ee97796ffe674ac62a347475768a3866da060
Yaroslav Benin <dondublon1@yandex.ru>
2022-11-23 20:57:47 +0400

    Patch with gevent.

--- authorization/src/main.py
+++ authorization/src/main.py
@@ -1,5 +1,10 @@
+"""To connect, use http://, not https://"""
 from flask import Flask
 # from db.init import init_db # doesn't work yet
+from gevent import monkey
+monkey.patch_all()
+
+from gevent.pywsgi import WSGIServer
 
 app = Flask(__name__)
 
@@ -7,12 +12,14 @@ app = Flask(__name__)
 @app.route('/create-user')
 def create_user():
     """Accept login and password here, post method"""
-    return 'Create user'
+    return 'Create user+'
 
 
 def main():
     # init_db(app)
-    app.run()
+    # app.run()
+    http_server = WSGIServer(('127.0.0.1', 5000), app)
+    http_server.serve_forever()
 
 
 if __name__ == '__main__':
