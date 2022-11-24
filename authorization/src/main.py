99151631f89befac824d42f32ccb8ed4e8f90892
Maxim <dev@max.im>
2022-11-25 01:11:20 +0300

    Add docker-compose config to use in dev workflow

--- authorization/src/main.py
+++ authorization/src/main.py
@@ -1,18 +1,7 @@
 from flask import Flask
-from gevent import monkey
-from gevent.pywsgi import WSGIServer
 from api.v1.user import bp_user
 from db.init import init_db
 
-monkey.patch_all()
-
 app = Flask(__name__)
+init_db(app)
 app.register_blueprint(bp_user)
-
-def main():
-    init_db(app)
-    http_server = WSGIServer(('127.0.0.1', 5000), app)
-    http_server.serve_forever()
-
-if __name__ == '__main__':
-    main()
