5ebced5232c786df19d11f1ad18838905dbdedf0
Maxim <dev@max.im>
2022-11-27 22:24:22 +0300

    Add functional tests

---
+++ tests/functional/src/api/v1/test_user.py
@@ -0,0 +1,14 @@
+from http import HTTPStatus
+
+import pytest
+
+# All test coroutines will be treated as marked with this decorator.
+pytestmark = pytest.mark.asyncio
+
+
+async def test_user_create(make_request):
+    response = await make_request('post')(
+        'user/create',
+        json={'login': 'test_user', 'password': 'test_pass'},
+    )
+    assert response.status == HTTPStatus.OK
