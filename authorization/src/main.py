a379493230a1f451acfa96618629da77004cc8eb
D Kalb <kds-ru@list.ru>
2022-12-03 13:18:40 +0100

    base structure created

--- authorization/src/main.py
+++ authorization/src/main.py
@@ -1,9 +1,32 @@
 from api.v1 import bp
 from db.init import init_db
 from flask import Flask
+from flasgger import Swagger
 from flask_marshmallow import Marshmallow
 
 app = Flask(__name__)
 init_db(app)
 ma = Marshmallow(app)
 app.register_blueprint(bp)
+
+swagger = Swagger(
+    app,
+    template={
+        "swagger": "2.0",
+        "info": {
+            "title": "Auth service",
+            "version": "1.0",
+        },
+        "consumes": [
+            "application/json",
+        ],
+        "produces": [
+            "application/json",
+        ],
+    },
+)
+
+app.config["SWAGGER"] = {
+    "title": "Swagger JWT Authentiation App",
+    "uiversion": 3,
+}
