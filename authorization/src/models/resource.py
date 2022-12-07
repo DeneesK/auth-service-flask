63f8ef482cc7d477181b27a38aa9b4bba44e570f
Yaroslav Benin <dondublon1@yandex.ru>
2022-12-07 18:44:14 +0400

    + reference to role model.

--- authorization/src/models/resource.py
+++ authorization/src/models/resource.py
@@ -1,7 +1,22 @@
 import uuid
 
+from sqlalchemy import Table, Column, ForeignKey
+
 from db.orm import db_engine as db
 from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import Mapped, relationship
+from sqlalchemy import Enum
+
+from models.role import RoleModel
+
+
+resource_role_table = Table(
+    "resource_role_table",
+    db.Model.metadata,
+    Column("resources", ForeignKey("resources.id")),
+    Column("roles", ForeignKey("roles.id")),
+    Column("action", Enum("view", "delete", "edit", name='resource_action'))
+)
 
 
 class ResourceModel(db.Model):
@@ -16,3 +31,4 @@ class ResourceModel(db.Model):
 
     name = db.Column(db.String, unique=False, nullable=False)
 
+    role: Mapped[list[RoleModel]] = relationship('RoleModel', secondary=resource_role_table)
