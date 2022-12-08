768a67f04596f1bdb47f03f1eac90d40d89d7e82
Yaroslav Benin <dondublon1@yandex.ru>
2022-12-08 19:11:14 +0400

    Work on notes.

--- authorization/src/api/v1/role.py
+++ authorization/src/api/v1/role.py
@@ -5,20 +5,17 @@ from schemas.role import role_data
 from services.role import RoleService
 
 
-bp = Blueprint('role', __name__, url_prefix='/role')
+bp = Blueprint('roles', __name__, url_prefix='/roles')
 
 
 @bp.route('/create', methods=['POST'])
 def create():
     role_service = RoleService()
-    print('001')
     try:
-        print('hasattr', hasattr(request, 'get_json'))
-        print('get_data', request.get_data())
         data = request.get_json()
         role_name = data['role_name']
-        client_service_id = data['client_service_id']
-        role_obj = role_service.create(role_name=role_name, client_service_id=client_service_id)
+        client_id = data['client_id']
+        role_obj = role_service.create(role_name, client_id)
     except Exception as e:
         return jsonify({'message': str(e)}), HTTPStatus.INTERNAL_SERVER_ERROR
 
--- authorization/src/services/role.py
+++ authorization/src/services/role.py
@@ -3,9 +3,9 @@ from models.role import RoleModel
 
 
 class RoleService:
-    def create(self, role_name, client_service_id):
+    def create(self, role_name, client_id):
         new_role = RoleModel(name=role_name,
-                             client_service_id=client_service_id)
+                             client_service_id=client_id)
         db_engine.session.add(new_role)
         db_engine.session.commit()
         return new_role
