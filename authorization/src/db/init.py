4f16f3318c0b716d6b079e2e8e5255e08d14588a
Yaroslav Benin <dondublon1@yandex.ru>
2022-12-14 20:29:57 +0400

    Usef Flask logger.

--- authorization/src/db/init.py
+++ authorization/src/db/init.py
@@ -1,11 +1,8 @@
-import logging
 from flask import Flask
 
 from models import RoleModel
 from services.role import RoleService
 from .orm import db_engine
-
-logger = logging.getLogger()
 
 
 def init_db(app: Flask):
@@ -13,15 +10,15 @@ def init_db(app: Flask):
     app.app_context().push()
 
     db_engine.create_all()
-    create_init_roles()
+    create_init_roles(app)
     db_engine.session.commit()
 
 
-def create_init_roles():
+def create_init_roles(app: Flask):
     model = RoleModel()
     db_admin_role = model.query.filter_by(name='admin').first()
     if not db_admin_role:
         role_service = RoleService()
         role_service.create('admin', '00000000-0000-0000-0000-000000000000')
-        logger.info("The initial 'admin' role created")
+        app.logger.info("The initial 'admin' role created")
         db_engine.session.commit()
